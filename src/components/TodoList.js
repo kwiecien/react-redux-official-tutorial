d9a47612a30808b62eae8797d66c84e32b7e0f6c
kwiecien <k.d.kwiecinski@gmail.com>
2019-05-14 19:33:59 +0200

    connect the Components

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,28 @@
 import React from "react";
 import Todo from "./Todo";
+import { connect } from 'react-redux';
+import { getTodos } from "../redux/selectors";
 
 const TodoList = ({ todos }) => (
     <ul className="todo-list">
         {todos && todos.length
             ? todos.map((todo, index) => {
-                return <Todo key={`todo-${todo.id}`} todo={todo} />;
+                return <Todo key={`todo-${todo.id}`} todo={todo}/>;
             })
             : "No todos, yay!"}
     </ul>
 );
 
-export default TodoList;
+const mapStateToProps = state => {
+    const { byIds, allIds } = state.todos || {};
+    const todos =
+        allIds && allIds.length
+            ? allIds.map(id => (byIds ? { ...byIds[id], id } : null))
+            : null;
+    return { todos };
+};
+
+export default connect(
+    // mapStateToProps,
+    state => ({ todos: getTodos(state) })
+)(TodoList);
